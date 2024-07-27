7c6d13c66cdd72b551800c2572a079eed5a53fee
VovaDev777 <polvovater@gmail.com>
2024-07-27 23:44:06 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,9 @@
-import {configureStore}
+import { configureStore } from "@reduxjs/toolkit";
+
+const rootReducer = (state = initialState, action) => {
+    return state;
+} 
+
+export const store = configureStore({
+    reducer: rootReducer,
+});
