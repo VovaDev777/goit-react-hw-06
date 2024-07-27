fb2f0834a18eca8e20d48a17cb3bd895221d74a8
VovaDev777 <polvovater@gmail.com>
2024-07-27 23:39:55 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '../src/components/App/App'
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider>
+      <App />
+    </Provider>
+  </React.StrictMode>
 )
