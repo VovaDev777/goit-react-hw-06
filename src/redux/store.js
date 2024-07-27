902a86e188cfeb0565ee316c655f252e158bc001
VovaDev777 <polvovater@gmail.com>
2024-07-27 23:49:26 +0200

    1

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+const initialState = 0;
+
 const rootReducer = (state = initialState, action) => {
     return state;
 } 
+
+
 
 export const store = configureStore({
     reducer: rootReducer,
