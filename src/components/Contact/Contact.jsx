8240fa6c2d4d0c83e747974f1d469dd266b2090b
VovaDev777 <polvovater@gmail.com>
2024-07-30 16:12:22 +0200

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,23 +2,21 @@ import css from './Contact.module.css'
 import { AiOutlineUser, AiFillPhone } from "react-icons/ai";
 import 'normalize.css';
 
+const Contact = ({ contact, onDelete }) => {
 
-
-const Contact = ({contact, onDelete}) => {
-  
   return (
     <div className={css.container}>
-        <ul className={css.list}>
-            <li className={css.listItem}>
-                <AiOutlineUser className={css.svgIcon}/>
-                <p className={css.text}>{contact.username}</p>
-            </li>
-            <li className={css.listItem}>
-                <AiFillPhone className={css.svgIcon}/>
-                <p className={css.text}>{contact.number}</p>
-            </li>
-        </ul>
-        <button type='submit' onClick={onDelete} className={css.btn}>Delete</button>
+      <ul className={css.list}>
+        <li className={css.listItem}>
+          <AiOutlineUser className={css.svgIcon} />
+          <p className={css.text}>{contact.username}</p>
+        </li>
+        <li className={css.listItem}>
+          <AiFillPhone className={css.svgIcon} />
+          <p className={css.text}>{contact.number}</p>
+        </li>
+      </ul>
+      <button type='submit' onClick={onDelete} className={css.btn}>Delete</button>
     </div>
   )
 }
