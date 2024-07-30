810b7edfea6cade02d4f23d925c73af3622b5bde
VovaDev777 <polvovater@gmail.com>
2024-07-30 16:12:34 +0200

    Update SearchBox.jsx

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -6,20 +6,19 @@ const SearchBox = () => {
 
   const dispatch = useDispatch();
   const filter = useSelector(selectNameFilter);
-
   const handleSearchChange = event => {
     dispatch(changeFilter(event.target.value));
   };
 
   return (
     <form className={css.form}>
-        <label className={css.label}>Find contacts by name</label>
-        <input 
-          type="text" 
-          className={css.input}
-          value={filter} 
-          onChange={handleSearchChange} 
-          />
+      <label className={css.label}>Find contacts by name</label>
+      <input
+        type="text"
+        className={css.input}
+        value={filter}
+        onChange={handleSearchChange}
+      />
     </form>
   )
 }
