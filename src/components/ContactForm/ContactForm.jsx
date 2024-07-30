1c4412eb7b98361f7408beb1257c2d19a0fedd7c
VovaDev777 <polvovater@gmail.com>
2024-07-30 16:12:05 +0200

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -7,55 +7,53 @@ import { addContact } from '../../redux/contactsSlice';
 
 const UserSchema = Yup.object().shape({
     username: Yup.string()
-     .min(3, 'Your name is too Short!')
-     .max(50, 'Your name is too Long!')
-     .required('This field is required'),
+        .min(3, 'Your name is too Short!')
+        .max(50, 'Your name is too Long!')
+        .required('This field is required'),
     number: Yup.string()
-     .min(2, 'Your number is too Short!')
-     .max(25, 'Your number is too Long!')
-     .required('This field is required'),
+        .min(2, 'Your number is too Short!')
+        .max(25, 'Your number is too Long!')
+        .required('This field is required'),
 });
-
-
 
 const ContactFrom = () => {
 
     const dispatch = useDispatch();
 
-  const handleSubmit = (values, { resetForm }) => {
-    dispatch(addContact({ id: Date.now().toString(), ...values }));
-    resetForm();
-  };
+    const handleSubmit = (values, { resetForm }) => {
+        dispatch(addContact({ id: Date.now().toString(), ...values }));
+        resetForm();
+    };
 
-  return (
-    <>
-    <Formik
-        initialValues={{
-            username: "",
-            number: "",
-        }}
-        onSubmit={handleSubmit}
-        validationSchema={UserSchema}
-    >
-        <Form>
-            <div className={css.form}>
-            <div className={css.formList}>
-                <div className={css.formItem}>
-                    <label className={css.label}>Name</label>
-                    <Field type="text" name="username" className={css.input} />
-                </div>
-                <div className={css.formItem}>
-                    <label className={css.label}>Number</label>
-                    <Field type="text" name="number" className={css.input} />
-                </div>
-            </div>
-            <button className={css.submitBtn} type="submit">Add contact</button>
-            </div>
-        </Form>
+    return (
+        <>
+            <Formik
+                initialValues={{
+                    username: "",
+                    number: "",
+                }}
+                onSubmit={handleSubmit}
+                validationSchema={UserSchema}
+            >
+                <Form>
+                    <div className={css.form}>
+                        <div className={css.formList}>
+                            <div className={css.formItem}>
+                                <label className={css.label}>Name</label>
+                                <Field type="text" name="username" className={css.input} />
+                            </div>
+                            <div className={css.formItem}>
+                                <label className={css.label}>Number</label>
+                                <Field type="text" name="number" className={css.input} />
+                            </div>
+                        </div>
+                        <button className={css.submitBtn} type="submit">Add contact</button>
+                    </div>
+                </Form>
 
-    </Formik>
-    </>
-  )
+            </Formik>
+        </>
+    )
 }
 
 export default ContactFrom
